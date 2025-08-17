c0c049cf2cedac65f79bf300c164f231e9d8a955
KingoBoiii <kingodk@outlook.dk>
2025-08-17 12:44:33 +0200

    feat: add Visual Studio build metadata for Windows
    Included "Hazel-Runtime.aps", "Hazel-Runtime.rc", and "resource.h" in the project configuration. This enhances the build process for Windows environments.

--- test/premake5.lua
+++ test/premake5.lua
@@ -26,6 +26,13 @@ project "Dingo-TestFramework"
 		systemversion "latest"
 		defines { "DE_PLATFORM_WINDOWS", }
 
+		-- Visual Studio build metadata
+		files {
+			"Hazel-Runtime.aps",
+			"Hazel-Runtime.rc",
+			"resource.h"
+		}
+
 	filter "system:linux"
 		defines { "DE_PLATFORM_LINUX" }
 
