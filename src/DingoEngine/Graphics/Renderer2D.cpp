2b863eeb6f54c3eafde45d0e38f9c2bee40b6a62
KingoBoiii <kingodk@outlook.dk>
2025-08-17 10:09:58 +0200

    refactor: simplify texture sampling logic
    Removed commented-out switch-case code for texture sampling. Replaced it with a single line that uses a non-uniform index and retains the discard condition for fully transparent colors.

--- src/DingoEngine/Graphics/Renderer2D.cpp
+++ src/DingoEngine/Graphics/Renderer2D.cpp
@@ -54,45 +54,6 @@ layout (set = 0, binding = 2) uniform sampler u_Sampler;
 
 void main()
 {
-	//vec4 texColor = v_Color;
-	//
-	//switch(int(v_TexIndex))
-	//{
-	//	case  0: texColor *= texture(sampler2D(u_Textures[ 0], u_Sampler), v_TexCoord); break;
-	//	case  1: texColor *= texture(sampler2D(u_Textures[ 1], u_Sampler), v_TexCoord); break;
-	//	case  2: texColor *= texture(sampler2D(u_Textures[ 2], u_Sampler), v_TexCoord); break;
-	//	case  3: texColor *= texture(sampler2D(u_Textures[ 3], u_Sampler), v_TexCoord); break;
-	//	case  4: texColor *= texture(sampler2D(u_Textures[ 4], u_Sampler), v_TexCoord); break;
-	//	case  5: texColor *= texture(sampler2D(u_Textures[ 5], u_Sampler), v_TexCoord); break;
-	//	case  6: texColor *= texture(sampler2D(u_Textures[ 6], u_Sampler), v_TexCoord); break;
-	//	case  7: texColor *= texture(sampler2D(u_Textures[ 7], u_Sampler), v_TexCoord); break;
-	//	case  8: texColor *= texture(sampler2D(u_Textures[ 8], u_Sampler), v_TexCoord); break;
-	//	case  9: texColor *= texture(sampler2D(u_Textures[ 9], u_Sampler), v_TexCoord); break;
-	//	case 10: texColor *= texture(sampler2D(u_Textures[10], u_Sampler), v_TexCoord); break;
-	//	case 11: texColor *= texture(sampler2D(u_Textures[11], u_Sampler), v_TexCoord); break;
-	//	case 12: texColor *= texture(sampler2D(u_Textures[12], u_Sampler), v_TexCoord); break;
-	//	case 13: texColor *= texture(sampler2D(u_Textures[13], u_Sampler), v_TexCoord); break;
-	//	case 14: texColor *= texture(sampler2D(u_Textures[14], u_Sampler), v_TexCoord); break;
-	//	case 15: texColor *= texture(sampler2D(u_Textures[15], u_Sampler), v_TexCoord); break;
-	//	case 16: texColor *= texture(sampler2D(u_Textures[16], u_Sampler), v_TexCoord); break;
-	//	case 17: texColor *= texture(sampler2D(u_Textures[17], u_Sampler), v_TexCoord); break;
-	//	case 18: texColor *= texture(sampler2D(u_Textures[18], u_Sampler), v_TexCoord); break;
-	//	case 19: texColor *= texture(sampler2D(u_Textures[19], u_Sampler), v_TexCoord); break;
-	//	case 20: texColor *= texture(sampler2D(u_Textures[20], u_Sampler), v_TexCoord); break;
-	//	case 21: texColor *= texture(sampler2D(u_Textures[21], u_Sampler), v_TexCoord); break;
-	//	case 22: texColor *= texture(sampler2D(u_Textures[22], u_Sampler), v_TexCoord); break;
-	//	case 23: texColor *= texture(sampler2D(u_Textures[23], u_Sampler), v_TexCoord); break;
-	//	case 24: texColor *= texture(sampler2D(u_Textures[24], u_Sampler), v_TexCoord); break;
-	//	case 25: texColor *= texture(sampler2D(u_Textures[25], u_Sampler), v_TexCoord); break;
-	//	case 26: texColor *= texture(sampler2D(u_Textures[26], u_Sampler), v_TexCoord); break;
-	//	case 27: texColor *= texture(sampler2D(u_Textures[27], u_Sampler), v_TexCoord); break;
-	//	case 28: texColor *= texture(sampler2D(u_Textures[28], u_Sampler), v_TexCoord); break;
-	//	case 29: texColor *= texture(sampler2D(u_Textures[29], u_Sampler), v_TexCoord); break;
-	//	case 30: texColor *= texture(sampler2D(u_Textures[30], u_Sampler), v_TexCoord); break;
-	//	case 31: texColor *= texture(sampler2D(u_Textures[31], u_Sampler), v_TexCoord); break;
-	//}
-	//o_Color = texColor;
-
 	o_Color = texture(sampler2D(u_Textures[nonuniformEXT(int(v_TexIndex))], u_Sampler), v_TexCoord) * v_Color;
 
 	if (o_Color.a == 0.0)
